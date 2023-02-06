7b3bd345e4fe2a3d73809037c6fe6dda48447b17
Nattaphak Udomwasurat <nattaphak.udom@kmutt.ac.th>
2023-02-06 20:12:28 +0700

    Khor 2

--- khor2/Program.cs
+++ khor2/Program.cs
@@ -8,6 +8,24 @@
         double Py = double.Parse(Console.ReadLine());
         int n = int.Parse(Console.ReadLine());
 
-        double r = Math.Sqrt(Math.Pow((Cx-Px),2) + Math.Pow((Cy-Py),2)); 
+        double r = Math.Sqrt(Math.Pow((Cx-Px),2) + Math.Pow((Cy-Py),2));
+
+        double theta = Math.Atan((Math.Abs(Cy-Py)/Math.Abs(Cx-Px))) ;
+
+        for(int i = n ; i > 0 ; i--)
+        {
+            r = r/2;
+
+            Px = ( r * 2 ) * ( Math.Cos ( theta + ( Math.PI / 2 ) ) ) + ( Cx );
+            Py = ( r * 2 ) * ( Math.Sin ( theta + ( Math.PI / 2 ) ) )+ ( Cy );
+
+            Cx = r * ( Math.Cos ( theta + ( Math.PI / 2 ) ) ) + ( Cx ) ;
+            Cy = r * ( Math.Sin ( theta + ( Math.PI / 2 ) ) ) + ( Cy ) ;
+
+            theta = theta + ( Math.PI / 2);
+
+        }
+
+        Console.WriteLine(Cx);Console.WriteLine(Cy);Console.WriteLine(Px);Console.WriteLine(Py);
     }
 } 
